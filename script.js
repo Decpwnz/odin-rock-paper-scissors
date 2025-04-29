e25bd589abb19205b67ebe0aecd08115f2d564da
Marijus Ladanovski <electromenn@gmail.com>
2025-04-29 19:28:24 +0300

    Create play rock paper scissors 5 times

--- script.js
+++ script.js
@@ -11,12 +11,85 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(input) {
-  // 1. Ask user to enter prompt
   userInput = prompt(input);
-
-  // 2. return entered prompt
   return userInput;
 }
 
-console.log(`Computer choice: ${getComputerChoice()}`);
-console.log(`User input: ${getHumanChoice()}`);
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    function playRound(humanChoice, computerChoice) {
+      if (
+        humanChoice.toLowerCase() === "Rock".toLowerCase() &&
+        computerChoice === "Paper"
+      ) {
+        console.log(`Player[R] < [P] Computer`);
+        computerScore++;
+      }
+      if (
+        humanChoice.toLowerCase() === "Paper".toLowerCase() &&
+        computerChoice === "Paper"
+      ) {
+        console.log(`Player[P] === [P] Computer`);
+      }
+      if (
+        humanChoice.toLowerCase() === "Scissors".toLowerCase() &&
+        computerChoice === "Paper"
+      ) {
+        console.log(`Player[S] > [P] Computer`);
+        humanScore++;
+      }
+
+      if (
+        humanChoice.toLowerCase() === "Paper".toLowerCase() &&
+        computerChoice === "Rock"
+      ) {
+        console.log(`Player[P] > [R] Computer`);
+        humanScore++;
+      }
+      if (
+        humanChoice.toLowerCase() === "Rock".toLowerCase() &&
+        computerChoice === "Rock"
+      ) {
+        console.log(`Player[R] === [R] Computer.`);
+      }
+      if (
+        humanChoice.toLowerCase() === "Scissors".toLowerCase() &&
+        computerChoice === "Rock"
+      ) {
+        console.log(`Player[S] < [R] Computer`);
+        computerScore++;
+      }
+
+      if (
+        humanChoice.toLowerCase() === "Paper".toLowerCase() &&
+        computerChoice === "Scissors"
+      ) {
+        console.log(`Player[P] < [S] Computer`);
+        computerScore++;
+      }
+      if (
+        humanChoice.toLowerCase() === "Rock".toLowerCase() &&
+        computerChoice === "Scissors"
+      ) {
+        console.log(`Player[R] > [S] Computer`);
+        humanScore++;
+      }
+      if (
+        humanChoice.toLowerCase() === "Scissors".toLowerCase() &&
+        computerChoice === "Scissors"
+      ) {
+        console.log(`Player[S] === [S] Computer`);
+      }
+    }
+    playRound(humanSelection, getComputerChoice());
+  }
+  console.log(`Human score: ${humanScore}`);
+  console.log(`Computer score: ${computerScore}`);
+}
+
+playGame();
